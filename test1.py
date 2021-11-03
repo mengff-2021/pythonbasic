387d92e62d569dcd4533308192d5d4159a99651a
mengff-2021 <you@example.com>
2021-11-04 07:12:47 +0800

    add spin def

--- test1.py
+++ test1.py
@@ -1,2 +1,26 @@
 
+def spin(data,i):
+    print(data,len(data),i)
+    n = len(data)
+    index = 0
+    while index < i:
+        data = spinData(data,n)
+        index = index + 1
+    print( "complete", data)
+
+def spinData(data,n):
+    index = 0
+    temp = ""
+    while index < n - 1:
+        if 0 == index:
+            temp = data[index]
+        data[index] = data[index + 1]
+        index = index+1
+    data[n-1] = temp
+    print(data)
+    return data
+    
 print ("Hello world")
+
+data = ["a","b","c","d","e","f","g","h"]
+spin(data,3)
