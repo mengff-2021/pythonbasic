95ea9fd197921bc8beb21791ba174d92fb4fdd19
meng.ff <goodfuwu25@yeah.net>
2021-11-05 23:31:13 +0800

    add new spin2

--- test1.py
+++ test1.py
@@ -19,8 +19,23 @@ def spinData(data,n):
     data[n-1] = temp
     print(data)
     return data
+
+def spin2(data,i):
+    index = 0
+    j = 0
+    n = len(data)
+    tempdata = data[:]
+    
+    while index < n:
+        if index < i:            
+           tempdata[n-i+index] = data[index]
+        else:
+           tempdata[j] = data[index]
+           j = j + 1
+        index = index + 1
+    print(tempdata)
     
 print ("Hello world")
 
 data = ["a","b","c","d","e","f","g","h"]
-spin(data,3)
+spin2(data,3)
